fcc4546c9faef38495516513fb886984abc08c73
ppseprus <p.seprus@gmail.com>
2017-05-05 01:54:36 +0200

    Create a simple `toggleState()` method

--- background.js
+++ background.js
@@ -1,37 +1,24 @@
-var active = true;
+var isEnabled = false;
 
-function setIcon() {
-  if (active) {
-    chrome.browserAction.setIcon({ path: 'icon_enabled.png' });
-  } else {
-    chrome.browserAction.setIcon({ path: 'icon_disabled.png' });
-  }
+function toggleState() {
+  isEnabled = !isEnabled;
+  chrome.browserAction.setIcon({ path: isEnabled ? 'icon_enabled.png' : 'icon_disabled.png' });
 }
 
-function changeState() {
-  if (active) {
-    active = false;
-  } else {
-    active = true;
-  }
-
-  setIcon();
-}
+chrome
+  .browserAction
+  .onClicked
+  .addListener(toggleState);
 
 chrome
   .runtime
   .onMessage
   .addListener(function (request, sender, sendResponse) {
     if (request.msg === 'getStatus') {
-      sendResponse({ status: active });
+      sendResponse({ status: isEnabled });
       return true;
     }
   });
-
-chrome
-  .browserAction
-  .onClicked
-  .addListener(changeState);
 
 chrome
   .tabs
@@ -42,4 +29,4 @@ chrome
       .executeScript(null, { file: 'intercept.js' });
   });
 
-setIcon();
+toggleState();
